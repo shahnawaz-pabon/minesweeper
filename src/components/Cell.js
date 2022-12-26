d420d9b3b37122dd32ec87eaaa8847bd1dd7c079
shahnawaz-pabon <s.pabon93@gmail.com>
2022-12-26 23:04:08 +0600

    cell component added

---
+++ src/components/Cell.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Cell = (props) => {
+  let className =
+    "cell" +
+    (props.value.isRevealed ? "" : " hidden") +
+    (props.value.isMine ? " is-mine" : "") +
+    (props.value.isFlagged ? " is-flag" : "");
+
+  const getValue = () => {
+    if (!props.value.isRevealed) {
+      return props.value.isFlagged ? "🇧🇩" : null;
+    }
+    if (props.value.isMine) {
+      return "💣";
+    }
+    if (props.value.neighbour === 0) {
+      return null;
+    }
+    return props.value.neighbour;
+  };
+
+  return (
+    <div
+      ref="cell"
+      onClick={props.onClick}
+      className={className}
+      onContextMenu={props.cMenu}
+    >
+      {getValue()}
+    </div>
+  );
+};
