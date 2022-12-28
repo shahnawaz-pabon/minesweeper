941ccd6b4e8b5d40bd9ca905f3243e0b045ea16c
shahnawaz-pabon <s.pabon93@gmail.com>
2022-12-28 22:04:26 +0600

    2d array for tracking mines initialized with setting mines

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,6 +10,45 @@ const Board = (props) => {
   const [gameStatus, setGameStatus] = useState(false);
   const [mineCount, setMineCount] = useState(props.mines);
 
+  const initializeArray = (height, width) => {
+    let data = [];
+    for (let i = 0; i < height; i++) {
+      data.push([]);
+      for (let j = 0; j < width; j++) {
+        data[i][j] = {
+          x: i,
+          y: j,
+          isMine: false,
+          neighbour: 0,
+          isRevealed: false,
+          isEmpty: false,
+          isFlagged: false,
+        };
+      }
+    }
+    return data;
+  };
+
+  // get random number given a dimension
+  const getRandomNumber = (dimension) => {
+    return Math.floor(Math.random() * 1000 + 1) % dimension;
+  };
+
+  const setMines = (data, height, width, mines) => {
+    let randomX,
+      randomY,
+      minesPut = 0;
+    while (minesPut < mines) {
+      randomX = getRandomNumber(width);
+      randomY = getRandomNumber(height);
+      if (!data[randomX][randomY].isMine) {
+        data[randomX][randomY].isMine = true;
+        minesPut++;
+      }
+    }
+    return data;
+  };
+
   const initBoardData = (height, width, mines) => {
     return {};
   };
