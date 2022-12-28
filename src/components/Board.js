42be1eeef2edb2f4f63b368ddb9f7e49198004a7
shahnawaz-pabon <s.pabon93@gmail.com>
2022-12-28 22:51:57 +0600

    neighbor's cells counted with traversing the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -29,28 +29,91 @@ const Board = (props) => {
     return data;
   };
 
-  // get random number given a dimension
-  const getRandomNumber = (dimension) => {
+  const getRandomPosition = (dimension) => {
     return Math.floor(Math.random() * 1000 + 1) % dimension;
   };
 
   const setMines = (data, height, width, mines) => {
     let randomX,
       randomY,
-      minesPut = 0;
-    while (minesPut < mines) {
-      randomX = getRandomNumber(width);
-      randomY = getRandomNumber(height);
+      totalMinesPlaced = 0;
+    while (totalMinesPlaced < mines) {
+      randomX = getRandomPosition(width);
+      randomY = getRandomPosition(height);
       if (!data[randomX][randomY].isMine) {
         data[randomX][randomY].isMine = true;
-        minesPut++;
+        totalMinesPlaced++;
       }
     }
     return data;
   };
 
+  const getNeighbours = (data, height, width) => {
+    let updatedData = data;
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        if (data[i][j].isMine !== true) {
+          let mine = 0;
+          const area = traverseBoard(data[i][j].x, data[i][j].y, data);
+          area.map((value) => {
+            if (value.isMine) {
+              mine++;
+            }
+          });
+          if (mine === 0) {
+            updatedData[i][j].isEmpty = true;
+          }
+          updatedData[i][j].neighbour = mine;
+        }
+      }
+    }
+    return updatedData;
+  };
+
+  // looks for neighbouring cells and returns them
+  const traverseBoard = (x, y, data) => {
+    const el = [];
+    //up
+    if (x > 0) {
+      el.push(data[x - 1][y]);
+    }
+    //down
+    if (x < props.height - 1) {
+      el.push(data[x + 1][y]);
+    }
+    //left
+    if (y > 0) {
+      el.push(data[x][y - 1]);
+    }
+    //right
+    if (y < props.width - 1) {
+      el.push(data[x][y + 1]);
+    }
+    // top left
+    if (x > 0 && y > 0) {
+      el.push(data[x - 1][y - 1]);
+    }
+    // top right
+    if (x > 0 && y < props.width - 1) {
+      el.push(data[x - 1][y + 1]);
+    }
+    // bottom right
+    if (x < props.height - 1 && y < props.width - 1) {
+      el.push(data[x + 1][y + 1]);
+    }
+    // bottom left
+    if (x < props.height - 1 && y > 0) {
+      el.push(data[x + 1][y - 1]);
+    }
+    return el;
+  };
+
   const initBoardData = (height, width, mines) => {
-    return {};
+    let data = initializeArray(height, width);
+    data = setMines(data, height, width, mines);
+    data = getNeighbours(data, height, width);
+
+    return data;
   };
 
   const renderBoard = (data) => {};
@@ -62,7 +125,7 @@ const Board = (props) => {
         <br />
         <span className="info">{gameStatus}</span>
       </div>
-      {renderBoard(boardData)}
+      {/* {renderBoard(boardData)} */}
     </div>
   );
 };
