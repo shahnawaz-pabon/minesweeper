927be56bcae68d2059173ecedc6822877475abb7
shahnawaz-pabon <s.pabon93@gmail.com>
2023-01-08 22:58:26 +0600

    timer component added

---
+++ src/components/Timer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className="timer">⏰ {seconds.toString().padStart(3, "0")}</div>;
+};
+
+export default Timer;
